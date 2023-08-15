381f2486bdab55d70b4d135c12f70d8064cc9c0a
Rogerio <rogerio@asf.com>
2023-08-14 21:50:00 -0300

    Adicionando Jenkinsfileweb para Playbooks

--- .vagrant/bundler/global.sol
+++ .vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"c47a7ab041641b0208c2fc8dfadf11eab871ce3dfd74d84ca2715a15f522908f","vagrant_version":"2.3.4"}
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]]],"checksum":"99e15f91d9bdc24ad8d7ae4c040866d295013fa4c9999eaceefe31291f150184","vagrant_version":"2.3.4"}
